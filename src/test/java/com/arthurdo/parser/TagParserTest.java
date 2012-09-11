9dc219a03bb467bce034b67bb8039c281194c88b
Chris Bushell <c.bushell@gmail.com>
2012-09-11 15:57:42 -0700

    another test for TagParser

--- src/test/java/com/arthurdo/parser/TagParserTest.java
+++ src/test/java/com/arthurdo/parser/TagParserTest.java
@@ -2,21 +2,35 @@ package com.arthurdo.parser;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.arthurdo.utils.CharUtils;
 
 public class TagParserTest {
 
-	@Test(expected = HtmlException.class)
-	public void itShouldThrowAnExceptionWhenBufferIsEmpty() throws HtmlException, IOException {
+	private TagParser tagParser;
+
+	@Before
+	public void setup() {
 		CharUtils charUtils = new CharUtils();
-		TagParser tagParser = new TagParser(charUtils);
-		
-		HtmlTag tag = new HtmlTag();
+		tagParser = new TagParser(charUtils);
+	}
+
+	@Test(expected = HtmlException.class)
+	public void itShouldThrowAnExceptionWhenBufferIsEmpty()
+			throws HtmlException, IOException {
 		StringBuffer sbuf = new StringBuffer();
-		
-		tagParser.parseTag(sbuf , tag);
+
+		tagParser.parseTag(sbuf, new HtmlTag());
+	}
+
+	@Test
+	public void itShouldNotThrowAnExceptionWhenBufferIsNotEmpty()
+			throws HtmlException, IOException {
+		StringBuffer sbuf = new StringBuffer("Foo");
+
+		tagParser.parseTag(sbuf, new HtmlTag());
 	}
 
 }
